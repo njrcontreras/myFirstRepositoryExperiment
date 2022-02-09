02ae0b92192af173eb1a507760f5d2ef904f687a
njrcontreras <njrcontreras@signetproperties.ph>
2022-02-09 16:47:21 +0800

    my first commit adding everyting on a blank c sharp app

---
+++ sampleGitRepoApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World again");
+            Console.WriteLine("Hello World again again");
+            Console.WriteLine("Hello World again again again");
+        }
+    }
+
+}
