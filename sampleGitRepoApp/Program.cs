b4d509022a1c41bb3b9d861db8e00ca7791f3ede
njrcontreras <njrcontreras@signetproperties.ph>
2022-02-10 11:02:26 +0800

    second branch of my cloned project

--- sampleGitRepoApp/Program.cs
+++ sampleGitRepoApp/Program.cs
@@ -13,6 +13,7 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World this is my second commit");
             Console.WriteLine("Hello World this is my third commit");
             Console.WriteLine("My first attempt to use git push command");
+            Console.WriteLine("My second attempt to push with a branch");
         }
     }
 
